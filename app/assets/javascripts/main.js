981d2c211e800c0288f0f44560bbf06c6504dec2
Vadym Motsukh <vadym.motsukh@perfectial.com>
2014-06-20 13:20:30 +0300

    first commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+var app = angular.module('testModule', []);
+
+app.controller('FirstCrt', function FirstCtrl() {
+
+});
+
+app.controller('SecondCrt', function SecondCrt() {
+
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAngularApp::Application.config.secret_token = '429bc38ae3c8c04b6cbc51213993a4bcbee37ce99b8759b51138f33f1cf65305acc41f7f0bb65ebc699861d25d3ed9d32655c956f7f80330783fc20d41e91344'
